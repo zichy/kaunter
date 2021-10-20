fce2d55ea809ddb746a7f76d8efa1fc023855823
zichy <zichy@zichy.de>
2021-10-20 20:18:35 +0200

    refactor button

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Counter
 class Counter {
 	constructor($el) {
 		this.$el  = $el;
@@ -5,7 +6,6 @@ class Counter {
 		this.$life = $el.querySelector('.counter__life');
 		let lifeCount = parseInt(this.$life.value, 10);
 		lifeCount = isNaN(lifeCount) ? 0 : lifeCount;
-
 
 		this.$buttons.forEach(($button) => {
 			$button.addEventListener('click', (e) => {
@@ -19,26 +19,30 @@ class Counter {
 	}
 }
 
-const $counter = document.querySelectorAll('.counter');
-
-$counter.forEach(($counter) => {
+document.querySelectorAll('.counter').forEach(($counter) => {
 	new Counter($counter);
 });
 
-const maxPlayers = 6;
+// Add player button
+class addButton {
+	constructor($el) {
+		this.$el = $el;
+		this.$players = document.querySelector('.players');
+		this.playerTemplate = document.querySelector('.player-template').content;
+		this.maxPlayers = 6;
 
-const $players = document.querySelector('.players');
-let playerTemplate = document.querySelector('.player-template').content;
+		this.$el.addEventListener('click', () => {
+			const currentPlayers = document.querySelectorAll('.player').length;
 
-const $playerButton = document.querySelector('.player-button');
-$playerButton.addEventListener('click', () => {
-	const currentPlayers = document.querySelectorAll('.player').length;
+			if(currentPlayers < this.maxPlayers) {
+				const clone = document.importNode(this.playerTemplate, true);
+				this.$players.appendChild(clone);
 
-	if(currentPlayers < maxPlayers) {
-		const clone = document.importNode(playerTemplate, true);
-		$players.appendChild(clone);
-
-		const $currentCounter = $players.querySelector('.player:last-of-type');
-		new Counter($currentCounter);
+				const $currentCounter = this.$players.querySelector('.player:last-of-type');
+				new Counter($currentCounter);
+			}
+		});
 	}
-});
+}
+
+new addButton(document.querySelector('.add-button'));
