6e7f9eeb86d69dfc13b2f8ef389906142af000ae
zichy <zichy@zichy.de>
2021-10-20 20:09:42 +0200

    refactor everything

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
-const $players = document.querySelector('.players');
-let playerTemplate = document.querySelector('.player-template').content;
-const clone = document.importNode(playerTemplate, true);
-$players.appendChild(clone);
+class Counter {
+	constructor($el) {
+		this.$el  = $el;
+		this.$buttons = $el.querySelectorAll('.counter__button');
+		this.$life = $el.querySelector('.counter__life');
+		let lifeCount = parseInt(this.$life.value, 10);
+		lifeCount = isNaN(lifeCount) ? 0 : lifeCount;
+
+
+		this.$buttons.forEach(($button) => {
+			$button.addEventListener('click', (e) => {
+				e.preventDefault();
+
+				const number = parseInt($button.dataset.count);
+				lifeCount = lifeCount + number;
+				this.$life.value = lifeCount;
+			});
+		});
+	}
+}
 
 const $counter = document.querySelectorAll('.counter');
 
-$counter.forEach((counter) => {
-	const $buttons = counter.querySelectorAll('.counter__button');
-	const $life = counter.querySelector('.counter__life');
-	let lifeCount = parseInt($life.value, 10);
-	lifeCount = isNaN(lifeCount) ? 0 : lifeCount;
-	
-	$buttons.forEach(($button) => {
-		const number = parseInt($button.dataset.count);
+$counter.forEach(($counter) => {
+	new Counter($counter);
+});
 
-		$button.addEventListener('click', (e) => {
-			e.preventDefault();
+const maxPlayers = 6;
 
-			lifeCount = lifeCount + number;
-			$life.value = lifeCount;
-		});
-	});
+const $players = document.querySelector('.players');
+let playerTemplate = document.querySelector('.player-template').content;
+
+const $playerButton = document.querySelector('.player-button');
+$playerButton.addEventListener('click', () => {
+	const currentPlayers = document.querySelectorAll('.player').length;
+
+	if(currentPlayers < maxPlayers) {
+		const clone = document.importNode(playerTemplate, true);
+		$players.appendChild(clone);
+
+		const $currentCounter = $players.querySelector('.player:last-of-type');
+		new Counter($currentCounter);
+	}
 });
