85aa7488a0ae2a8e1bac136e8948f1f8cbffec2d
zichy <zichy@zichy.de>
2021-10-21 21:49:05 +0200

    fix basically everything

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-// Counter
-class Counter {
+// Alert on reload
+window.onbeforeunload = function (ev) {
+	var ev = ev || window.event;
+	const warning = 'Do you want to leave this page? Your counters will not be saved.';
+
+	if (ev) {
+		ev.returnValue = warning;
+	}
+
+	return warning;
+};
+
+// Life counter
+class LifeCounter {
 	constructor($el) {
 		this.$el  = $el;
-		this.$buttons = $el.querySelectorAll('.counter__button');
-		this.$life = $el.querySelector('.counter__life');
+		this.$buttons = $el.querySelectorAll('.life__button');
+		this.$life = $el.querySelector('.life__input');
 
 		let lifeCount = parseInt(this.$life.value, 10);
 		lifeCount = isNaN(lifeCount) ? 0 : lifeCount;
@@ -20,25 +32,29 @@ class Counter {
 	}
 }
 
-document.querySelectorAll('.counter').forEach(($counter) => {
-	new Counter($counter);
-});
-
-// Player
-class Player {
+// Secondary counters
+class SecondaryCounters {
 	constructor($el) {
 		this.$el = $el;
-		this.$content = document.querySelector('.content');
-		this.$intro = document.querySelector('.intro');
-		this.playerTemplate = document.querySelector('.player-template').content;
+		this.$counters = $el.querySelectorAll('.counter__item');
 
-		const clone = document.importNode(this.playerTemplate, true);
-		this.$el.appendChild(clone);
+		this.$counters.forEach(($counter) => {
+			const $input = $counter.querySelector('.counter__input');
+			const $buttons = $counter.querySelectorAll('.counter__button');
 
-		const $currentCounter = this.$content.querySelector('.player:last-of-type');
-		new Counter($currentCounter);
+			let count = parseInt($input.value, 10);
+			count = isNaN(count) ? 0 : count;
 
-		this.$el.appendChild(this.$intro);
+			$buttons.forEach(($button) => {
+				$button.addEventListener('click', (e) => {
+					e.preventDefault();
+
+					const number = parseInt($button.dataset.count);
+					count = count + number;
+					$input.value = count;
+				});
+			});
+		});
 	}
 }
 
@@ -54,27 +70,80 @@ class DeleteButton {
 	}
 }
 
-// Add button
-class AddButton {
-	constructor($el) {
+// Reset button
+class ResetButton {
+	constructor($el, $input, life) {
 		this.$el = $el;
-		this.$button = this.$el.querySelector('.add-button');
-		this.$content = document.querySelector('.content');
-		this.maxPlayers = 6;
+		this.$input = $input;
+		this.life = life;
+		this.$counters = $el.closest('.player').querySelectorAll('.counter__input');
 
-		this.$button.addEventListener('click', () => {
-			const currentPlayers = document.querySelectorAll('.player').length;
-			if(currentPlayers < this.maxPlayers) {
-				new Player(this.$content);
-			}
+		this.$el.addEventListener('click', (e) => {
+			e.preventDefault();
+			$input.value = life;
 
-			const $currentPlayer = this.$content.querySelector('.player:last-of-type');
-			// $currentPlayer.focus();
-
-			const $currentDeleteButton = this.$content.querySelector('.player:last-of-type .player__delete');
-			new DeleteButton($currentDeleteButton);
+			this.$counters.forEach(($counter) => {
+				$counter.value = 0;
+			});
 		});
 	}
 }
 
-new AddButton(document.querySelector('.intro'));
+// Add button
+class AddButtons {
+	constructor($el) {
+		this.$el = $el;
+		this.$buttons = this.$el.querySelectorAll('.add-button');
+		this.$content = document.querySelector('.content');
+		this.maxPlayers = 6;
+
+		this.$buttons.forEach(($button) => {
+			const startingLife = $button.dataset.life;
+
+			$button.addEventListener('click', () => {
+				const currentPlayers = document.querySelectorAll('.player').length;
+				if(currentPlayers < this.maxPlayers) {
+					new Player(this.$content, startingLife);
+				}
+			});
+		});
+	}
+}
+
+new AddButtons(document.querySelector('.intro'));
+
+// Player
+class Player {
+	constructor($el, life) {
+		this.$el = $el;
+		this.life = life;
+		this.$content = document.querySelector('.content');
+		this.$intro = document.querySelector('.intro');
+
+		// Get player template
+		this.playerTemplate = document.querySelector('.player-template');
+		this.clone = document.importNode(this.playerTemplate.content, true);
+		this.$el.appendChild(this.clone);
+
+		// Initialize counters
+		this.$currentPlayer = this.$content.querySelector('.player:last-of-type');
+		new LifeCounter(this.$currentPlayer);
+		new SecondaryCounters(this.$currentPlayer);
+
+		// Set life count
+		this.$currentLife = this.$currentPlayer.querySelector('.life__input');
+		this.$currentLife.value = this.life;
+
+		// Initialize delete button
+		this.$currentDeleteButton = this.$currentPlayer.querySelector('.player__delete');
+		new DeleteButton(this.$currentDeleteButton);
+
+		// Initialize reset button
+		this.$currentResetButton = this.$currentPlayer.querySelector('.counter__reset');
+		new ResetButton(this.$currentResetButton, this.$currentLife, this.life);
+
+		// Insert template
+		this.$el.appendChild(this.$intro);
+		this.$currentPlayer.focus();
+	}
+}
